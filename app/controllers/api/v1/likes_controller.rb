90f2c6c532ff1868a42d851e6d03e2d5205d3117
emyrue <94935633+emyrue@users.noreply.github.com>
2023-07-24 17:36:23 -0600

    Removed location of like from create like method in likes controller

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::LikesController < ApplicationController
     @like = Like.new(like_params)
 
     if @like.save
-      render json: @like, status: :created, location: @like
+      render json: @like, status: :created
     else
       render json: @like.errors, status: :unprocessable_entity
     end
@@ -47,6 +47,6 @@ class Api::V1::LikesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def like_params
-    params.require(:like).permit(:like)
+    params.require(:like).permit(:like, :user_id, :review_id)
   end
 end
